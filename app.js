27c066a31f45ea87c7bdf5372ab73e76361823a0
Kudrik Karina <karina.muratowa2012@yandex.ru>
2022-08-23 10:19:55 +0300

    logger, cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
+const { allowedCors } = require('./middlewares/allowedCors');
 const { createUser, login } = require('./controllers/users');
 const routesUsers = require('./routes/users');
 const routesCards = require('./routes/cards');
@@ -9,6 +11,7 @@ const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 const { handleErrors } = require('./errors/handleErrors');
 const { regex } = require('./models/regex');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -18,6 +21,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors(allowedCors));
+app.use(requestLogger);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -40,6 +46,8 @@ app.use(auth);
 
 app.use('/cards', routesCards);
 app.use('/users', routesUsers);
+
+app.use(errorLogger);
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
---
+++ middlewares/allowedCors.js
@@ -0,0 +1,9 @@
+module.exports.allowedCors = {
+  origin: [
+    'http://karinakudrik.mesto.nomoredomains.sbs',
+    'https://karinakudrik.mesto.nomoredomains.sbs',
+    'http://localhost:3000',
+    'https://localhost:3000',
+  ],
+  credentials: true,
+};
